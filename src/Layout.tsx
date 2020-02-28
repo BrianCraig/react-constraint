d0c6e4fa4dfcf3431908c50f26a03480eb69fae9
Brian Craig <briancraigok@gmail.com>
2020-02-28 11:38:41 -0300

    feat: cloning passed Element

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,15 @@
-import React, { useContext, FunctionComponent } from "react";
+import React, {
+  useContext,
+  FunctionComponent,
+  ReactElement,
+  ReactNode
+} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
-import { createLayoutComponent } from "./constraints/generator";
+import { createLayoutComponent, DebugElement } from "./constraints/generator";
 import { vertices } from "./examples/vertices";
 import { LayoutContext, LayoutProvider } from "./LayoutContext";
+import { width } from "@material-ui/system";
 
 const F = "";
 
@@ -88,7 +94,20 @@ const ComponentList: FunctionComponent<{ box: string; item: string }> = ({
 const ComponentView = () => {
   const { layout, addComponent } = useContext(LayoutContext);
   const Comp = createLayoutComponent(layout);
-  return <Comp width={400} height={400} />;
+  const props: { [key: string]: ReactNode } = {};
+  Object.keys(layout).forEach(name => {
+    props[name] = (
+      <DebugElement style={{ width: 0, height: 0, top: 0, left: 0 }} />
+    );
+  });
+  return (
+    <Comp
+      width={400}
+      height={400}
+      parent={<DebugElement style={{ width: 0, height: 0, top: 0, left: 0 }} />}
+      {...props}
+    />
+  );
 };
 
 export const Layout: React.FC = () => {
--- src/constraints/generator.tsx
+++ src/constraints/generator.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { ReactElement, FunctionComponent } from "react";
 import { LayoutDefinition, Side, ComponentInstance } from "./definition";
 import { ParseLayout } from "./parser";
 import { ResolveConstraints } from "./constraint";
+import { findComponentByName } from "./util";
 
 export interface LayoutComponent {
   width: number;
@@ -9,25 +10,29 @@ export interface LayoutComponent {
   [key: string]: React.ReactNode;
 }
 
-const ComponentToNode = ({ positions, name }: ComponentInstance) => {
-  const width = (positions[Side.right] || 0) - (positions[Side.left] || 0);
-  const height = (positions[Side.bottom] || 0) - (positions[Side.top] || 0);
+interface DebugElementInterface {
+  style: { width: number; height: number; top: number; left: number };
+}
+
+export const DebugElement: FunctionComponent<DebugElementInterface> = ({
+  style: { width, height, top, left }
+}) => {
   return (
     <div
       style={{
         position: "absolute",
-        top: positions[Side.top],
-        left: positions[Side.left],
         background: "#00000022",
-        width: width,
-        height: height,
+        width,
+        height,
+        top,
+        left,
         border: "1px solid #000",
         boxSizing: "border-box",
         fontSize: 12
       }}
       children={
         <>
-          {`${name}`}
+          {`PASS NAME TO DEBUG`}
           <br />
           {`${width} * ${height}`}
         </>
@@ -51,5 +56,18 @@ export const createLayoutComponent = (
 
   ResolveConstraints(constraints);
 
-  return <div>{componentsInstances.map(ComponentToNode)}</div>;
+  const getComponent = ({ positions, name }: ComponentInstance) => {
+    const width = (positions[Side.right] || 0) - (positions[Side.left] || 0);
+    const height = (positions[Side.bottom] || 0) - (positions[Side.top] || 0);
+    return React.cloneElement(components[name] as ReactElement, {
+      style: {
+        width,
+        height,
+        top: positions[Side.top],
+        left: positions[Side.left]
+      }
+    });
+  };
+
+  return <div>{componentsInstances.map(getComponent)}</div>;
 };
